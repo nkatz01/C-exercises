f1388503b7f30c27d254ac70df0fd48900d456a1
nuchemka <nuchemka@gmail.com>
2020-10-25 20:53:30 +0000

    shortened superflous code in flr function

--- CodeSignal/Program.cs
+++ CodeSignal/Program.cs
@@ -11,7 +11,7 @@ namespace CodeSignal
             //  duplicateCharacters("Aasdefsgh!!!"); program that returns number of characters that have duplicates - 1 for each group of duplicates (i.e.  !!! = ! = 1)
            //Console.WriteLine( secondHighestDigit("234367"));//program that returns second highest digit in a string, ignoring non-numeric characters. it returns -1 if only one or none digit was given.
            //each number is treated individualy. So, if the highest number has a duplicate, then this number is retruned as the second highest.
-          // Console.WriteLine( flr("LFRFRFR"));//(Test with: RF -> 3,FLLF -> 0,LFRFRFR -> 1)//program that returns minimum number of commands (steps and head turns) for a robat to retrun to original starting point on an (x,y) plain. 
+           Console.WriteLine( flr("FLLF"));//(Test with: RF -> 3,FLLF -> 0,LFRFRFR -> 1)//program that returns minimum number of commands (steps and head turns) for a robat to retrun to original starting point on an (x,y) plain. 
           //Parameter: a string of commands, ignoring lower letters and anything not L, R or F. L = turn head left, R = turne head right, F = make one step forward. 
         }
 
@@ -62,79 +62,94 @@ namespace CodeSignal
                 }
             }
             int minCommands = 0;
-            minCommands = Math.Abs(coordinates.Item1 + coordinates.Item2);
+            minCommands = Math.Abs(coordinates.Item1) + Math.Abs(coordinates.Item2);
             var x = coordinates.Item1;
             var y = coordinates.Item2;
-            if (x == 0 && y == 0)
+            if ((x > 0 && direction == 1) || (x < 0 && direction == 3))
+               return minCommands += 2;
+            else if ((y > 0 && direction == 0) || (y < 0 && direction == 2))
+               return minCommands += 2;
+            else if ((x == 0 && y > 0 && direction == 2) || (x == 0 && y < 0 && direction == 0) || (x == 0 && y == 0))
                 return minCommands;
-            else if (x == 0 || y == 0)
-            {
-
-                if (x > 0)
-                {
-                    if (direction == 0 || direction == 2)
-                        minCommands++;
-                    else if (direction == 1)
-                        minCommands += 2;
-                    else;//dir = 3
-                }
-                else if (y > 0)
-                {
-                    if (direction == 1 || direction == 3)
-                        minCommands++;
-                    else if (direction == 0)
-                        minCommands += 2;
-                    else;//dir = 2
-                }
-                else if (x < 0)
-                    if (direction == 0 || direction == 2)
-                        minCommands++;
-                    else if (direction == 3)
-                        minCommands += 2;
-                    else;//dir = 1
-                else // y<0 && x == 0
-                {
-                    if (direction == 3 || direction == 1)
-                        minCommands++;
-                    else if (direction == 2)
-                        direction += 2;
-                    else; //dir = 0
-                }
-
-            }
-            else   {// neither of them (x nor y) are 0
-                if (x > 0 && y > 0)
-                {
-                    if (direction == 0 || direction == 1)
-                        minCommands += 2;
-                    else
-                        minCommands++;
-                }
-                else if (x < 0 && y > 0)
-                {
-                    if (direction == 0 || direction == 3)
-                        minCommands += 2;
-                    else
-                        minCommands++;
-                }
-                else if (x < 0 && y < 0)
-                    if (direction == 3 || direction == 2)
-                        minCommands += 2;
-                    else
-                        minCommands++;
-                else //y is negative and x is positive
-                    if (direction == 1 || direction == 2)
-                    minCommands += 2;
-                else
-                    minCommands++;
-
-            }
-
-            return minCommands;
+            else
+                return 1; 
 
 
 
 
+
+
+
+            //if (x == 0 && y == 0)
+            //    return minCommands;
+            //else if (x == 0 || y == 0)
+            //{
+
+            //    if (x > 0)
+            //    {
+            //        if (direction == 0 || direction == 2)
+            //            minCommands++;
+            //        else if (direction == 1)
+            //            minCommands += 2;
+            //        else;//dir = 3
+            //    }
+            //    else if (y > 0)
+            //    {
+            //        if (direction == 1 || direction == 3)
+            //            minCommands++;
+            //        else if (direction == 0)
+            //            minCommands += 2;
+            //        else;//dir = 2
+            //    }
+            //    else if (x < 0)
+            //        if (direction == 0 || direction == 2)
+            //            minCommands++;
+            //        else if (direction == 3)
+            //            minCommands += 2;
+            //        else;//dir = 1
+            //    else // y<0 && x == 0
+            //    {
+            //        if (direction == 3 || direction == 1)
+            //            minCommands++;
+            //        else if (direction == 2)
+            //            direction += 2;
+            //        else; //dir = 0
+            //    }
+
+            //}
+            //else   {// neither of them (x nor y) are 0
+            //    if (x > 0 && y > 0)
+            //    {
+            //        if (direction == 0 || direction == 1)
+            //            minCommands += 2;
+            //        else
+            //            minCommands++;
+            //    }
+            //    else if (x < 0 && y > 0)
+            //    {
+            //        if (direction == 0 || direction == 3)
+            //            minCommands += 2;
+            //        else
+            //            minCommands++;
+            //    }
+            //    else if (x < 0 && y < 0)
+            //        if (direction == 3 || direction == 2)
+            //            minCommands += 2;
+            //        else
+            //            minCommands++;
+            //    else //y is negative and x is positive
+            //        if (direction == 1 || direction == 2)
+            //        minCommands += 2;
+            //    else
+            //        minCommands++;
+
+            //}
+
+            //return minCommands;
+
+
+
+            //Pseudocode:
             //(Note: 0 = north, 3 = west)
             //if either x or y is 0, than the number of commands is the abs value of the one (x xor y) that isn't 0, plus:
             //if x > 0
